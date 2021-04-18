4d7df93bd4d87d536609e737c406d1b0a8c3b202
Manish Anand <manishanandprogrammer@gmail.com>
2021-04-18 20:57:09 +0530

    Clean up Factorial Class

--- src/main/java/optimizing_recursion/tailcalls/Factorial.java
+++ src/main/java/optimizing_recursion/tailcalls/Factorial.java
@@ -3,11 +3,15 @@ package optimizing_recursion.tailcalls;
 import static optimizing_recursion.tailcalls.TailCalls.call;
 
 public class Factorial{
-    public static TailCall factorialTailRec(final int factorial, final int number) {
+    private static TailCall<Integer> factorialTailRec(final int factorial, final int number) {
         if (number == 1)
             return TailCalls.done(factorial);
         else
             return call(() -> factorialTailRec(factorial * number, number - 1));
+    }
+
+    public static int factorial(final int number) {
+        return factorialTailRec(1, number).invoke();
     }
 
 }
--- src/main/java/optimizing_recursion/tailcalls/MainTest.java
+++ src/main/java/optimizing_recursion/tailcalls/MainTest.java
@@ -2,8 +2,6 @@ package optimizing_recursion.tailcalls;
 
 public class MainTest {
     public static void main(String[] args) {
-        TailCall<Integer> tailCall = Factorial.factorialTailRec(1, 5);
-        int result = tailCall.invoke();
-        System.out.println(result);
+        System.out.println(Factorial.factorial(5));
     }
 }
